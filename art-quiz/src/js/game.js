1c5453333acdc9036d35dde64f0a55cdb43bc82b
Anna <missjellx@gmail.com>
2021-11-23 14:50:44 +0300

    feat: add timer

--- art-quiz/src/js/game.js
+++ art-quiz/src/js/game.js
@@ -29,16 +29,45 @@ const nextPictureBtn = document.querySelector('.next-picture');
 const nextQuizBtn = document.querySelector('.modal-next-quiz');
 const modalEndGame = document.querySelector('.modal-wrapper-end-game');
 const modalHomeLink = document.querySelector('.modal-home-link');
+const progressTime = document.querySelector('.progress-time');
+const progressLine = document.querySelector('.progress-line');
 
 function startGame(start, end, card) {
   let time = +getTime();
+  let gameEnd = false;
+  let percent = 100 / time;
+  let totalPercent = 100;
+
+  progressTime.textContent = `0:${String(time).padStart(2, '0')}`;
+  progressLine.style.background = `linear-gradient(to right, #ffbca2 0%, #ffbca2 100%, #a4a4a4 100%, #a4a4a4 100%)`;
+
+  let timer = setInterval(() => {
+    time--;
+    totalPercent -= percent;
+    
+    progressTime.textContent = `0:${String(time).padStart(2, '0')}`;
+    progressLine.style.background = `linear-gradient(to right, #ffbca2 0%, #ffbca2 ${totalPercent}%, #a4a4a4 ${totalPercent}%, #a4a4a4 100%)`;
+    if (
+      getCurrentBlock() !== picQuestion &&
+      getCurrentBlock() !== artistQuestion
+    ) {
+      gameEnd = true;
+      clearInterval(timer);
+    }
+  }, 1000);
 
   setTimeout(() => {
-    if (getCurrentBlock() === picQuestion || getCurrentBlock() === artistQuestion) {
+    if (
+      (getCurrentBlock() === picQuestion ||
+        getCurrentBlock() === artistQuestion) &&
+      !gameEnd
+    ) {
       endGame(getRightAnswers());
       endGameSound();
       addAnimationHide(modalAnswer);
+      gameEnd = false;
     }
+    clearInterval(timer);
   }, time * 1000);
 
   let currentCategory = getCurrentCategory();
