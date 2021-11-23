3b21fbcd2631188bfcd95e775a43dce35cde0028
Anna <missjellx@gmail.com>
2021-11-23 15:12:33 +0300

    feat: add switcher for timer

--- art-quiz/src/js/game.js
+++ art-quiz/src/js/game.js
@@ -23,7 +23,7 @@ import { renderPicQuestion } from './pic-question.js';
 import { renderCategories } from './category.js';
 import { renderArtistQuestion } from './artist-question.js';
 import { endGameSound } from './audio.js';
-import { getTime } from './settings.js';
+import { getSwitcher, getTime } from './settings.js';
 
 const nextPictureBtn = document.querySelector('.next-picture');
 const nextQuizBtn = document.querySelector('.modal-next-quiz');
@@ -31,44 +31,51 @@ const modalEndGame = document.querySelector('.modal-wrapper-end-game');
 const modalHomeLink = document.querySelector('.modal-home-link');
 const progressTime = document.querySelector('.progress-time');
 const progressLine = document.querySelector('.progress-line');
+const progressBlock = document.querySelector('.question-progress');
 
 function startGame(start, end, card) {
-  let time = +getTime();
-  let gameEnd = false;
-  let percent = 100 / time;
-  let totalPercent = 100;
+  if (getSwitcher()) {
+    progressBlock.classList.remove('hidden');
+    let time = +getTime();
+    let gameEnd = false;
+    let percent = 100 / time;
+    let totalPercent = 100;
 
-  progressTime.textContent = `0:${String(time).padStart(2, '0')}`;
-  progressLine.style.background = `linear-gradient(to right, #ffbca2 0%, #ffbca2 100%, #a4a4a4 100%, #a4a4a4 100%)`;
-
-  let timer = setInterval(() => {
-    time--;
-    totalPercent -= percent;
-    
     progressTime.textContent = `0:${String(time).padStart(2, '0')}`;
-    progressLine.style.background = `linear-gradient(to right, #ffbca2 0%, #ffbca2 ${totalPercent}%, #a4a4a4 ${totalPercent}%, #a4a4a4 100%)`;
-    if (
-      getCurrentBlock() !== picQuestion &&
-      getCurrentBlock() !== artistQuestion
-    ) {
-      gameEnd = true;
-      clearInterval(timer);
-    }
-  }, 1000);
+    progressLine.style.background = `linear-gradient(to right, #ffbca2 0%, #ffbca2 100%, #a4a4a4 100%, #a4a4a4 100%)`;
 
-  setTimeout(() => {
-    if (
-      (getCurrentBlock() === picQuestion ||
-        getCurrentBlock() === artistQuestion) &&
-      !gameEnd
-    ) {
-      endGame(getRightAnswers());
-      endGameSound();
-      addAnimationHide(modalAnswer);
-      gameEnd = false;
-    }
-    clearInterval(timer);
-  }, time * 1000);
+    let timer = setInterval(() => {
+      time--;
+      totalPercent -= percent;
+
+      progressTime.textContent = `0:${String(time).padStart(2, '0')}`;
+      progressLine.style.background = `linear-gradient(to right, #ffbca2 0%, #ffbca2 ${totalPercent}%, #a4a4a4 ${totalPercent}%, #a4a4a4 100%)`;
+      console.log(getCurrentBlock());
+      if (
+        getCurrentBlock() !== picQuestion &&
+        getCurrentBlock() !== artistQuestion
+      ) {
+        gameEnd = true;
+        clearInterval(timer);
+      }
+    }, 1000);
+
+    setTimeout(() => {
+      if (
+        (getCurrentBlock() === picQuestion ||
+          getCurrentBlock() === artistQuestion) &&
+        !gameEnd
+      ) {
+        endGame(getRightAnswers());
+        endGameSound();
+        addAnimationHide(modalAnswer);
+        gameEnd = false;
+      }
+      clearInterval(timer);
+    }, time * 1000);
+  } else {
+    progressBlock.classList.add('hidden');
+  }
 
   let currentCategory = getCurrentCategory();
 
