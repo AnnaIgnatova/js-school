ab30b28def957c5b7c662184b53e4a20fa2c12af
Anna <missjellx@gmail.com>
2021-12-12 13:21:43 +0300

    init: start project

---
+++ christmas-task/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  extends: [
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["react", "@typescript-eslint", "jest", "import"],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["tsconfig.eslint.json", "tsconfig.json"],
+  },
+  ignorePatterns: ["babel.config.js"],
+  rules: {
+    "linebreak-style": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
+};
---
+++ christmas-task/src/components/header/Header.tsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <div>avrurgnruirgnuieruingiui</div>;
+}
+
+export default Header;
